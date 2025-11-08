7a98ef56526f4b1eb36914761ad4a0162bee4eca
Ghutra <156716842+Ghutra@users.noreply.github.com>
2025-11-07 17:01:05 -0800

    Update stock.js

--- assets/data/stock.js
+++ assets/data/stock.js
@@ -1,1 +1,103 @@
+[
+  {
+    "name": "1121 Sella Basmati Rice",
+    "origin": "Pakistan",
+    "packaging": "4×10 = 40kg Nonwoven with Transparent Master Bag (Old Crop)",
+    "price": "200 AED",
+    "size": "40kg",
+    "stock": "650 bags",
+    "badge": "Verified Supplier",
+    "img": "basmati-pak.jpg"
+  },
+  {
+    "name": "1121 Sella Basmati Rice",
+    "origin": "India",
+    "packaging": "4×10 = 40kg Nonwoven with Transparent Master Bag (Old Crop)",
+    "price": "160 AED",
+    "size": "40kg",
+    "stock": "1500 bags",
+    "badge": "Peer Rated",
+    "img": "basmati-india.jpg"
+  },
+  {
+    "name": "1121 Sella Basmati Rice",
+    "origin": "India",
+    "packaging": "4×10 = 40kg Nonwoven with Transparent Master Bag (Old Crop)",
+    "price": "150 AED",
+    "size": "40kg",
+    "stock": "600 bags",
+    "badge": "Verified Supplier",
+    "img": "basmati-india.jpg"
+  },
+  {
+    "name": "Golden Sella Basmati Rice",
+    "origin": "India",
+    "packaging": "4×10 = 40kg Nonwoven with Transparent Master Bag (Old Crop)",
+    "price": "130 AED",
+    "size": "40kg",
+    "stock": "650 bags",
+    "badge": "Peer Rated",
+    "img": "golden-sella.jpg"
+  },
+  {
+    "name": "Sona Massori",
+    "origin": "India",
+    "packaging": "18kg Nonwoven",
+    "price": "40 AED",
+    "size": "18kg",
+    "stock": "1500 bags",
+    "badge": "Verified Supplier",
+    "img": "sona-massori.jpg"
+  },
+  {
+    "name": "Irri 6 White Rice Broken 5%",
+    "origin": "Pakistan",
+    "packaging": "35kg PP Bags",
+    "price": "60 AED",
+    "size": "35kg",
+    "stock": "3000 bags",
+    "badge": "Verified Supplier",
+    "img": "irri6-5.jpg"
+  },
+  {
+    "name": "Irri 6 White Rice Broken 100%",
+    "origin": "Pakistan",
+    "packaging": "35kg PP Bags",
+    "price": "51 AED",
+    "size": "35kg",
+    "stock": "1500 bags",
+    "badge": "Peer Rated",
+    "img": "irri6-100.jpg"
+  },
+  {
+    "name": "1509 Creamy Sella Rice",
+    "origin": "India",
+    "packaging": "10×4 = 40kg Nonwoven (New Crop)",
+    "price": "780 USD C&F",
+    "size": "40kg",
+    "stock": "Booking only",
+    "badge": "Pre-Booking",
+    "img": "1509-creamy.jpg"
+  },
+  {
+    "name": "Irri 6 White Rice Broken 5%",
+    "origin": "Pakistan",
+    "packaging": "35kg PP Bags",
+    "price": "350 USD C&F",
+    "size": "35kg",
+    "stock": "Prompt shipment",
+    "badge": "Pre-Booking",
+    "img": "irri6-5.jpg"
+  },
+  {
+    "name": "Irri 6 White Rice Broken 100%",
+    "origin": "Pakistan",
+    "packaging": "35kg PP Bags",
+    "price": "335 USD C&F",
+    "size": "35kg",
+    "stock": "Prompt shipment",
+    "badge": "Pre-Booking",
+    "img": "irri6-100.jpg"
+  }
+]
 
