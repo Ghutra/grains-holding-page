fc9d70d6ad932385f0e837486c78f8ca04081a3d
Ghutra <156716842+Ghutra@users.noreply.github.com>
2025-11-13 15:11:24 -0800

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+// firebase-config.js
+export const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "grains-hub.firebaseapp.com",
+  projectId: "grains-hub",
+  storageBucket: "grains-hub.appspot.com",
+  messagingSenderId: "1234567890",
+  appId: "1:1234567890:web:abcdef1234567890"
+};
