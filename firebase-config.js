f3a4958e5229717f8b6150a5d30b2aea66db91ae
Ghutra <156716842+Ghutra@users.noreply.github.com>
2025-11-15 16:05:02 -0800

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,21 @@
-// firebase-config.js
-export const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBz2Ha_cj42LI-Dh4VYkEOOsROt2WE9fDU",
   authDomain: "grains-hub.firebaseapp.com",
   projectId: "grains-hub",
-  storageBucket: "grains-hub.appspot.com",
-  messagingSenderId: "1234567890",
-  appId: "1:1234567890:web:abcdef1234567890"
+  storageBucket: "grains-hub.firebasestorage.app",
+  messagingSenderId: "783882873254",
+  appId: "1:783882873254:web:ba93eaaff5eca25cd6c767",
+  measurementId: "G-RHYCMRHS1Q"
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
